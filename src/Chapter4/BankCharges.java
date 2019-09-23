7206927a45671fc491793c501dda75d7d4e4688d
benjaminp <benjamin7734@gmail.com>
2019-09-23 00:40:54 -0500

    Added methods in BankCharges class to calculate fees and modified the output of the statement string with a NumberFormat object.

--- src/Chapter4/BankCharges.java
+++ src/Chapter4/BankCharges.java
@@ -2,11 +2,65 @@ package Chapter4;
 
 import Chapter2.AskUserForInput;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class BankCharges {
     final private double monthlyCharge = 10;
-    private int checksUsed = 0;
+    private int checksUsed;
+    private double accountBalance;
 
-    
+
+    public String statement() {
+        NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
+        return "Amount of checks used this month = "+
+                getChecksUsed()+'\n'+
+                "Monthly account holding fee = "+
+                format.format(getMonthlyCharge())+'\n'+
+                "Your current account balance = "+
+                format.format(getAccountBalance())+'\n'+
+                "Your total fees for the month = "+
+                format.format(calculateFees());
+    }
+
+    //region Methods used to calculate the fees acquired from
+    // both account balance and checks used.
+    /**
+     * Sums the doubles returned from calculateBalanceFee,
+     * calculateCheckFee, and the monthlyCharge field;
+     * @return A double, the sum of all fees acquired for the month.
+     */
+    public double calculateFees() {
+        return (calculateCheckFees() + calculateBalanceFee() + getMonthlyCharge());
+    }
+
+    /**
+     * Return the appropriate decimal based upon the accountBalance field.
+     * @return A double, either 15.00 or 0.00.
+     */
+    public double calculateBalanceFee() {
+        if (getAccountBalance() < 400.00)
+            return 15.00;
+        else return 0.00;
+    }
+
+    /**
+     * Returns the appropriate decimal based upon the integer returned
+     * from checksUsed field.
+     * @return A double, checksUsed times the appropriate rate.
+     */
+    public double calculateCheckFees() {
+
+        if ( (getChecksUsed() > 0) && (getChecksUsed() <= 19) )
+            return ( getChecksUsed() * 0.10 );
+        else if( (getChecksUsed() > 19) && (getChecksUsed() <= 39))
+            return ( getChecksUsed() * 0.08 );
+        else if( (getChecksUsed() > 39) && (getChecksUsed() <= 59))
+            return ( getChecksUsed() * 0.06 );
+        else return ( getChecksUsed() * 0.04 );
+    }
+    //endregion
+
     //region Methods used to input to the checksUsed field.
     /**
      * This method is essentially a wrapper to tie all the input methods together.
@@ -48,6 +102,14 @@ public class BankCharges {
 
     //region Setters and getters, minus a setter for the monthlyCharge
     // variable which is a final variable
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
     public double getMonthlyCharge() {
         return monthlyCharge;
     }
--- src/Chapter4/BankChargesDriver.java
+++ src/Chapter4/BankChargesDriver.java
@@ -1,10 +1,27 @@
 package Chapter4;
 
 public class BankChargesDriver {
+    /**
+     * Entry point to application.
+     * @param args
+     */
     public static void main(String[] args) {
-
+        bankChargesDemo();
     }
-    public static void bankChargesDemo() {
 
+    /**
+     * Demonstrates an instance of the bankCharges class.
+     */
+    public static void bankChargesDemo() {
+        BankCharges charge0 = new BankCharges();
+
+        // Initialize the checksUsed field
+        charge0.assignChecksUsed();
+
+        // Initialize the accountBalance field
+        charge0.setAccountBalance(399.00);
+
+        // Display the bank statement
+        System.out.println(charge0.statement());
     }
 }
