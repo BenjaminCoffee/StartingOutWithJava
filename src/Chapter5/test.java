6630c18978a8e6da6b48a92e6324a17771a874ce
benjaminp <benjamin7734@gmail.com>
2019-12-18 00:00:33 -0600

    Added Chapter5 package to src folder in StartingOutWithJava. Added a test.java class to play around with new exercises on loops while reading Chapter 5 in the book.

---
+++ src/Chapter5/test.java
@@ -0,0 +1,72 @@
+package Chapter5;
+
+import Chapter2.AskUserForInput;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        /*
+        int x = 8, y;
+
+        y = x--;
+
+        System.out.println(x);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(x);
+
+         */
+
+        /*
+        Scanner keyboard = new Scanner(System.in);
+
+        int z;
+        z = keyboard.nextInt();
+        while(z < 10 || z > 25) {
+            System.out.println("The number you entered is out of range.");
+            System.out.println("Please try again.");
+            z = keyboard.nextInt();
+        }
+
+        System.out.println("The number you entered is "+ z);
+
+         */
+
+        /*
+        int i;
+
+        for(i=1; i <=49; i++) {
+            if (i % 2 !=0) {
+                System.out.println(i);
+            }
+        }
+
+        for(i = 0; i <= 100; i++) {
+            if(i%5 == 0) {
+                System.out.println(i);
+            }
+        }
+
+         */
+        /*
+        int sum = 0;
+        for(int i=0; i < 7; i++) {
+            sum =+AskUserForInput.
+                    askUserForInteger("Enter number"+(i)+ ": ");
+        }
+
+         */
+        for(int rows = 0; rows < 2; rows++ ) {
+            for(int columns=0; columns < 8; columns++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = 0; i < 8; i++) {
+
+        }
+    }
+}
