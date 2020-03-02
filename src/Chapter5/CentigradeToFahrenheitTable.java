2913a08d818328d974c127a764dcf56235bee7d9
benjaminp <benjamin7734@gmail.com>
2020-03-01 21:17:12 -0600

    Added CentigradeToFahrenheitTable.java and CentigradeToFahrenheitDriver.java to Chapter5 package.

---
+++ src/Chapter5/CentigradeToFahrenheitTable.java
@@ -0,0 +1,35 @@
+package Chapter5;
+
+public class CentigradeToFahrenheitTable {
+    private double centigrade;
+
+    public String calculateTableToTwentyDegrees() {
+
+        // create a string builder object
+        StringBuilder str = new StringBuilder();
+
+        // set centigrade to zero before the loop starts
+        setCentigrade(0);
+
+        for (int i = 0; i <= 20; i++) {
+           str.append(getCentigrade()+ " degrees centigrade = " +
+                   calculateCentigradeToFahrenheit() + " degrees" +
+                   " Fahrenheit."+ "\n");
+
+            setCentigrade(getCentigrade() + 1);
+        }
+        return str.toString();
+    }
+
+    public double calculateCentigradeToFahrenheit() {
+        return ( (getCentigrade() * (9.0/5.0) ) + 32.0);
+    }
+
+    public double getCentigrade() {
+        return centigrade;
+    }
+
+    public void setCentigrade(double centigrade) {
+        this.centigrade = centigrade;
+    }
+}
