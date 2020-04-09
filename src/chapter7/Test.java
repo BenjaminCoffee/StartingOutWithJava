72676b8c64e23b28b7f7aa57f42bdca4282a6f73
benjaminp <benjamin7734@gmail.com>
2020-04-09 01:41:48 -0500

    Testing a commit after instalation of latest jdk.

--- src/chapter7/Test.java
+++ src/chapter7/Test.java
@@ -2,6 +2,28 @@ package chapter7;
 
 public class Test {
     public static void main(String[] args) {
+        // 7.1
+        int[] employeeNumbers = new int[100];
+
+        // 7.2
+        double[] payRate = new double[25];
+
+        // 7.3
+        float[] miles = new float[14];
+
+        // 7.4
+        char[] letters = new char[1000];
+
+        // 7.7
+        // 1
+        // 2
+        // 3
+        // 4
+        // 5
+
+        // 7.8
+        double[] doubleArr = {1.7, 6.4, 8.9, 3.1, 9.2};
+
 
     }
 }
