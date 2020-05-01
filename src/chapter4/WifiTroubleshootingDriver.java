268d1c12f8bfd2678c3ba9bd651e3cf1614e1211
benjaminp <benjamin7734@gmail.com>
2020-04-30 23:36:51 -0500

    removed chapter4 package.

--- src/chapter4/WifiTroubleshootingDriver.java
+++
@@ -1,8 +0,0 @@
-package chapter4;
-
-public class WifiTroubleshootingDriver {
-    public static void main(String[] args) {
-        WifiTroubleshooting problem0 = new WifiTroubleshooting();
-        problem0.beginTroubleshooting();
-    }
-}
